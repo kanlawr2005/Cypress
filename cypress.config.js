956444aa0d7ce30eba42672c53618bd7847461f3
Robert Lawrence Ebinezer <84284541+kanlawr2005@users.noreply.github.com>
2022-08-03 13:19:22 +0530

    Update cypress.config.js
    updated version

--- cypress.config.js
+++ cypress.config.js
@@ -1,9 +1,12 @@
-const { defineConfig } = require("cypress");
-
-module.exports = defineConfig({
-  e2e: {
-    setupNodeEvents(on, config) {
-      // implement node event listeners here
-    },
-  },
-});
+customLaunchers: {
+  ChromeHeadless: {
+    base: 'Chrome',
+    flags: [
+      '--headless',
+      '--disable-gpu',
+      '--no-sandbox',
+      '--remote-debugging-port=9222',
+    ]
+  }
+},
+browsers: ['ChromeHeadless'],
